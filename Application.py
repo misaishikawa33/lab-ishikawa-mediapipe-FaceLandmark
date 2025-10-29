9441fe1a9fb59bd3df9b2e6d5364c32a4f6e5b7d
misaishikawa33 <ishikawa.misa.dj@tut.jp>
2025-10-29 14:14:24 +0900

    カメラ姿勢推定前にランドマークを調整

--- Application.py
+++ Application.py
@@ -253,22 +253,6 @@ class Application:
         # 画像の描画を実行
         #
         self.image.flags.writeable = True
-        
-        # 
-        # カメラ姿勢推定前にランドマークを調整
-        # (姿勢推定にも調整を反映させる)
-        #
-        if self.adjust_landmarks and self.use_facelandmark and self.face_mesh.multi_face_landmarks:
-            # 調整情報を先に計算
-            self.alignment_info = self.calculate_landmark_alignment()
-            if self.alignment_info:
-                # ランドマークを調整（元のデータを上書き）
-                import copy
-                for face_landmarks in self.face_mesh.multi_face_landmarks:
-                    face_landmarks.landmark[234].x = self.alignment_info['right_ear_target'][0] / self.width
-                    face_landmarks.landmark[234].y = self.alignment_info['right_ear_target'][1] / self.height
-                    face_landmarks.landmark[454].x = self.alignment_info['left_ear_target'][0] / self.width
-                    face_landmarks.landmark[454].y = self.alignment_info['left_ear_target'][1] / self.height
 
         # ランドマークの描画
         if self.draw_landmark:
@@ -338,6 +322,14 @@ class Application:
                 self.model_scale_factor = self.calculate_model_scale_from_ears()
             else:
                 self.model_scale_factor = 1.0
+            
+            #
+            # ランドマーク位置調整が有効な場合、調整情報を計算
+            #
+            if self.adjust_landmarks and self.use_facelandmark:
+                self.alignment_info = self.calculate_landmark_alignment()
+            else:
+                self.alignment_info = None
             
             #
             # マスク着用時、モデルを描画
@@ -458,14 +450,36 @@ class Application:
     def draw_landmarks(self, image):
         if self.face_mesh.multi_face_landmarks:
             for face_landmarks in self.face_mesh.multi_face_landmarks:
-                # 通常の描画（既にdisplay_func内で調整済み）
-                mp.solutions.drawing_utils.draw_landmarks(
-                    image,
-                    face_landmarks,
-                    # 描画モード
-                    mp.solutions.face_mesh.FACEMESH_TESSELATION,
-                    self.drawing_spec,
-                    self.drawing_spec)
+                # ランドマーク位置調整が有効な場合、234と454の座標を調整
+                if self.adjust_landmarks and self.alignment_info:
+                    # ランドマークのコピーを作成
+                    import copy
+                    landmarks_copy = copy.deepcopy(face_landmarks)
+                    
+                    # ランドマーク234（右端）をFaceDetectionの右耳に合わせる
+                    landmarks_copy.landmark[234].x = self.alignment_info['right_ear_target'][0] / self.width
+                    landmarks_copy.landmark[234].y = self.alignment_info['right_ear_target'][1] / self.height
+                    
+                    # ランドマーク454（左端）をFaceDetectionの左耳に合わせる
+                    landmarks_copy.landmark[454].x = self.alignment_info['left_ear_target'][0] / self.width
+                    landmarks_copy.landmark[454].y = self.alignment_info['left_ear_target'][1] / self.height
+                    
+                    mp.solutions.drawing_utils.draw_landmarks(
+                        image,
+                        landmarks_copy,
+                        # 描画モード
+                        mp.solutions.face_mesh.FACEMESH_TESSELATION,
+                        self.drawing_spec,
+                        self.drawing_spec)
+                else:
+                    # 通常の描画
+                    mp.solutions.drawing_utils.draw_landmarks(
+                        image,
+                        face_landmarks,
+                        # 描画モード
+                        mp.solutions.face_mesh.FACEMESH_TESSELATION,
+                        self.drawing_spec,
+                        self.drawing_spec)
     
     #
     # FaceLandmark検出結果を画像上に描画する関数（バウンディングボックスなし）
